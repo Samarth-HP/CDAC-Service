32707cb3513f7cf3fc1033c34855f3d5629bc2a1
ChakshuGautam <chaks.gautam@gmail.com>
2021-10-14 16:45:08 +0530

    Fix: Add credentials through env

--- cdac/src/main/java/com/samagra/cdac/CdacApplication.java
+++ cdac/src/main/java/com/samagra/cdac/CdacApplication.java
@@ -1,10 +1,15 @@
 package com.samagra.cdac;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CdacApplication {
+
+    @Value("username") String username;
+    @Value("senderId") String senderId;
+    @Value("password") String password;
 
     public static void main(String[] args) {
         SpringApplication.run(CdacApplication.class, args);
