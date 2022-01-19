3554616d0e88ee6b97c9d8084e99bfae05fa9bc7
Radhay Anand <radhay@samagragovernance.in>
2022-01-20 00:30:49 +0530

    added api for bulk sms thru csv

--- src/main/java/com/samagra/cdac/CdacApplication.java
+++ src/main/java/com/samagra/cdac/CdacApplication.java
@@ -1,5 +1,6 @@
 package com.samagra.cdac;
 
+import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import com.google.gson.Gson;
+import java.util.HashMap;
 
 @SpringBootApplication
 @RestController
@@ -76,6 +84,32 @@ public class CdacApplication {
         try {
             String resp = smsService.sendUnicodeOtpSMS(username, password, message, senderId, mobileNumber, secureKey, templateid);
             return new ResponseEntity<>(resp, HttpStatus.OK);
+        }catch (Exception e){
+            System.out.println("Exception: " + e);
+            return new ResponseEntity<>("ERROR", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/send_from_csv")
+    ResponseEntity<String> sendFromCsv(@RequestParam String message, @RequestParam("file") MultipartFile file, @RequestParam String templateid) {
+        try {
+            if (file.isEmpty()) {
+                return new ResponseEntity<>("ERROR UPLOADING CSV", HttpStatus.BAD_REQUEST);
+            } else {
+                HashMap<String, String> respMap = new HashMap<>();
+                Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        String resp = smsService.sendUnicodeOtpSMS(username, password, message, senderId, line, secureKey, templateid);
+                        respMap.put(line, resp);
+                    }
+                } catch (Exception ex) {
+                    return new ResponseEntity<>("ERROR PROCESSING CSV", HttpStatus.BAD_REQUEST);
+                }
+//                String resp = smsService.sendUnicodeOtpSMS(username, password, message, senderId, mobileNumber, secureKey, templateid);
+                return new ResponseEntity<>(gson.toJson(respMap), HttpStatus.OK);
+            }
         }catch (Exception e){
             System.out.println("Exception: " + e);
             return new ResponseEntity<>("ERROR", HttpStatus.NOT_FOUND);
